46b00214c6314ece98d595c89711f76be37c03fa
KaungMyatLin <kaungmyatlin.worklife@gmail.com>
2022-11-21 14:13:10 +0630

    u

---
+++ src/components/classCmpntTut/Counter.tsx
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+/* wants to show count value is 5. */
+export class Counter extends Component {
+    state = {
+        count: 0
+    }
+
+    handleClick = () => {
+        this.setState( prevState => ({count: prevState.count +1}))
+    },
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleClick}>Increment</button>
+                {this.props.message} {this.state.count}
+            </div>
+        )
+    }
+}
